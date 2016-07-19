30c7ae51cd5422835ec60c00fbd98c308b3b0d2f
hiwhitley <hiwhitley@163.com>
2016-07-19 20:05:57 +0800

    update

---
+++ src/main/java/com/hiwhitley/demo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.hiwhitley.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AmountView mAmountView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAmountView = (AmountView) findViewById(R.id.amount_view);
+        mAmountView.setGoods_storage(50);
+        mAmountView.setOnAmountChangeListener(new AmountView.OnAmountChangeListener() {
+            @Override
+            public void onAmountChange(View view, int amount) {
+                Toast.makeText(getApplicationContext(), "Amount=>  " + amount, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
